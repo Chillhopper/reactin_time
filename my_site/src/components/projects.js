2cf2561d862c7571c4a5025d483f15e723d8dbcd
Chillhopper <68851163+Chillhopper@users.noreply.github.com>
2023-08-07 23:08:00 +0800

    some additions
    stuff

--- my_site/src/components/projects.js
+++ my_site/src/components/projects.js
@@ -2,9 +2,22 @@ import React from 'react'
 import './projects.css'
 import { Link } from 'react-router-dom';
 
+const KEY = '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'
+
 function Projects(){
     return(
+        <>
         <h1>My Projects</h1>
+        <div class="projhead">
+            <button class="proj">tilt1</button>
+            <button class="proj">tilt1</button>
+            <button class="proj">tilt1</button>
+            <button class="proj">tilt1</button>
+            <button class="proj">tilt1</button>
+            <button class="proj">tilt1</button>
+        </div>
+        </>
+
     );
 }
 
